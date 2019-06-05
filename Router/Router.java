fa59fda156706e566be31430a7e10a6facfce442
spencerwooo <spencerwoo98@gmail.com>
2019-06-05 15:30:48 +0800

    Read and print router table

--- Router/Router.java
+++ Router/Router.java
@@ -10,24 +10,58 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Router {
-  private static File preferenceFile = new File("Router/preference.txt");
+  // 路由表文件位置（初始值）
+  private static File routerTablePrefFile = new File("Router/routerTable.txt");
 
-  public static List<List<String>> readPreference(File file) throws FileNotFoundException, IOException {
-    List<List<String>> preference = new ArrayList<List<String>>();
+  /**
+   * Read router table
+   *
+   * @param file 路由表文件
+   * @return List<List<String>> 路由表数据结构（二维列表）
+   */
+  public static List<List<String>> readRouterTable(File file) throws FileNotFoundException, IOException {
+    List<List<String>> routerTable = new ArrayList<List<String>>();
     BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
 
     String fileString;
     while ((fileString = bufferedReader.readLine()) != null) {
       List<String> strRow = Arrays.asList(fileString.split(" "));
-      preference.add(strRow);
+      routerTable.add(strRow);
     }
     bufferedReader.close();
 
-    return preference;
+    return routerTable;
+  }
+
+  /**
+   * 打印路由表
+   *
+   * @param routerTable
+   */
+  public static void printRouterTable(List<List<String>> routerTable) {
+    int routerTableRow = routerTable.size();
+    int routerTableCol = routerTable.get(0).size();
+
+    String[] routerName = {"A", "B", "C", "D", "E"};
+
+    System.out.println("------ Router Table -----------");
+    System.out.println(String.format("|    | %-2s | %-2s | %-2s | %-2s | %-2s |", "A", "B", "C", "D", "E"));
+    for (int i = 0; i < routerTableRow; i++) {
+      System.out.print(String.format("| %-2s |", routerName[i]));
+      for (int j = 0; j < routerTableCol; j++) {
+        System.out.print(String.format(" %-2s |", routerTable.get(i).get(j)));
+      }
+      System.out.println();
+    }
+    System.out.println("------ Router Table -----------");
   }
 
   public static void main(String[] args) throws FileNotFoundException, IOException {
-    List<List<String>> preference = Router.readPreference(preferenceFile);
-    System.out.println(preference);
+    System.out.println("------ Router Initialize ------");
+
+    List<List<String>> routerTable = Router.readRouterTable(routerTablePrefFile);
+    System.out.println(routerTable);
+
+    printRouterTable(routerTable);
   }
 }
